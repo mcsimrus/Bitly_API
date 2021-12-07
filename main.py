3500815d90d6ba080923b0d6d63b8c2c4f350a82
mcsimrus <mcsimrus@gmail.com>
2021-12-07 08:29:49 +0300

    Added other domains support

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ def count_clicks(short_input, headers):
     return clicks_count
 
 
-def shorten_link(user_input, headers):
+def shorten_link(user_input, headers, user_domain,user_group_guid):
 
-    data = {'long_url': user_input, 'domain': 'bit.ly', 'group_guid': 'Blb54sihppy'}
+    data = {'long_url': user_input, 'domain': user_domain, 'group_guid': user_group_guid}
     response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=data)
     response.raise_for_status()
     bitlink = response.json()['link']
@@ -40,6 +40,9 @@ def shorten_link(user_input, headers):
 def main():
     load_dotenv()
     token = os.environ['BITLY_API_TOKEN']
+    user_domain_dflt = os.environ['USER_DOMAIN']
+    user_group_guid_dflt = os.environ['USER_GROUP_GUID']
+    
     headers = {
             'Authorization': 'Bearer {}'.format(token),
             'Content-Type': 'application/json',
@@ -49,8 +52,12 @@ def main():
         description='Описание что делает программа'
     )
     parser.add_argument('user_input', help='ссылка пользователя')
+    parser.add_argument('-ud', default=user_domain_dflt, help='user domain')
+    parser.add_argument('-ugg', default=user_group_guid_dflt, help='user group guid')
     args = parser.parse_args()
     user_input = args.user_input
+    user_domain = args.ud
+    user_group_guid = args.ugg
 
     short_input = urlparse(user_input)._replace(scheme='').geturl()
 
@@ -58,7 +65,7 @@ def main():
         if is_bitlink(short_input, headers):
             print('Всего переходов', count_clicks(short_input, headers), 'раз(а)')
         else:
-            print('Битлинк', shorten_link(user_input, headers))
+            print('Битлинк', shorten_link(user_input, headers, user_domain, user_group_guid))
     except requests.exceptions.HTTPError as error:
         exit('Не могу загрузить данные из сервера:\n{0}'.format(error))
 
